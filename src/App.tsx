98ab6ac94faf273b37766b6b78d8c9bf2ae48fff
L Rakesh Singh <110081692+Rakezt@users.noreply.github.com>
2026-02-21 13:02:35 +0530

    initial UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Body from './Body';
+import Login from './Login';
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/' element={<Body />}>
+            <Route path='/login' element={<Login />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
