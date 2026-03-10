bfda5bbd67a66c4198fbe302870af6ca16634980
L Rakesh Singh <110081692+Rakezt@users.noreply.github.com>
2026-03-10 13:36:37 +0530

    updated BE api

--- src/utils/constant.tsx
+++ src/utils/constant.tsx
@@ -1,4 +1,6 @@
 export const BASE_URL =
-  location.hostname === 'localhost' ? 'http://localhost:7777' : '/api';
+  location.hostname === 'localhost'
+    ? 'http://localhost:7777'
+    : 'https://zingbe.onrender.com';
 
 // export const BASE_URL = '/api';
